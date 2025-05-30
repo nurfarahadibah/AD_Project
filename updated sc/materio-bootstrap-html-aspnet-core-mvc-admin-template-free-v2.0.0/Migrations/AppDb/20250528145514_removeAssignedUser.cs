0e5cd5dd489ea701b7179981881c4042b5ae8b05
nrulerina <nrulerina@gmail.com>
2025-05-30 10:11:20 +0800

    updatedsc

---
+++ updated sc/materio-bootstrap-html-aspnet-core-mvc-admin-template-free-v2.0.0/Migrations/AppDb/20250528145514_removeAssignedUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspnetCoreMvcFull.Migrations.AppDb
+{
+    /// <inheritdoc />
+    public partial class removeAssignedUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FolderUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FolderUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ComplianceFolderId = table.Column<int>(type: "int", nullable: false),
+                    AssignedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FolderUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FolderUsers_ComplianceFolders_ComplianceFolderId",
+                        column: x => x.ComplianceFolderId,
+                        principalTable: "ComplianceFolders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FolderUsers_ComplianceFolderId",
+                table: "FolderUsers",
+                column: "ComplianceFolderId");
+        }
+    }
+}
